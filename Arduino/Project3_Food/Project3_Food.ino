fbb2c135adc6c5d1a75f2d055ad8ae55754cc6ae
ashleywoon <54552775+ashleywoon@users.noreply.github.com>
2020-05-05 21:08:41 -0700

    V2 upload

--- Arduino/Project3_Food/Project3_Food.ino
+++ Arduino/Project3_Food/Project3_Food.ino
@@ -1,27 +1,34 @@
 /* Project 3 Food
  *  by Ashley Woon
  *  
+ *  Designed for Adafruit ESP32, Hardware components include: 
+ *  (1)breadboard, (1)push button, (1)potentiometer, (1)LED, (1) RGB LED, (5)resistors
  *  
- * 
+ *  This sketch was written to be run alongside the Processing sketch Project3_Food as the interactive elements.
+ *  Project3 simulates an imaginary machine that transforms small nutrution pellets into full meals that can be 
+ *  chosen via a "knob" which in this case refers to the potentiometer. A push button serves as a mean to confirm 
+ *  meal choice, open, and close a door to access the meal.
  */
-
+//LED pins
 const int rLED = 4;
 const int bLED = 5;
 const int gLED = 18;
-
-const int button = 12;
-int buttonState;
-
-const int pot = A2;
-int potValue;
-
 const int LED = 15;
 
+//analog pins and values
+const int button = 12;
+int buttonState;
+int lastButtonState;
+int buttonCount = 0;
+const int pot = A2;
+int potValue = 0;
+
+
 void setup() {
+  //initializing pins, inputs, and outputs
   pinMode(rLED, OUTPUT);
   pinMode(bLED, OUTPUT);
   pinMode(gLED, OUTPUT);
-
   pinMode(LED, OUTPUT);
 
   pinMode(button, INPUT);
@@ -31,7 +38,109 @@ void setup() {
 }
 
 void loop() {
-  buttonState = digitalRead(button);
-  potValue = analogRead(pot);
+  getButtonValue();
+  getPotValue();
+  sendSerialData();
+  checkButton();
+  changeFood();
+}
 
+void getButtonValue() {
+  buttonState = digitalRead(button);
+}
+
+void getPotValue() {
+  potValue = analogRead(pot);
+}
+
+void sendSerialData() {
+  // Add switch on or off
+  if(buttonState ) {
+    Serial.print(1);
+  }
+  else {
+    Serial.print(0);
+  }
+
+   Serial.print(",");
+   Serial.print(potValue);
+
+   Serial.print(",");
+   Serial.print(buttonCount);
+  // end with newline
+  Serial.println();
+}
+
+void checkButton() {
+  if(buttonState != lastButtonState) 
+      {
+        if(buttonState == HIGH)
+        {
+          buttonCount++;
+        }
+        if(buttonCount == 3)
+        {
+          buttonCount = 0;
+        }
+      }
+  lastButtonState = buttonState;
+}
+
+//Checks to see if button count is equal to 2 indicating a meal has been chosen, confirmed, and the user
+//now wants to open the door. An LED will stay on until the button is pressed again indicating the door has closed
+void checkDoor() {
+  if(buttonCount == 2)
+    {
+       digitalWrite(LED, HIGH);
+    }
+    else
+      digitalWrite(LED, LOW);
+}
+
+//This function changes states between default and meal choices based on potentiometer values
+void changeFood() {
+  if(potValue >= 0 && potValue <= 1025)
+    defaultState();
+  else if(potValue >= 1026 && potValue <= 2050)
+    chickenState();
+  else if(potValue >= 2051 && potValue <= 3075)
+    cowState();
+  else if(potValue >= 3076 && potValue <= 4095)
+    fishState();
+}
+
+//when the potentiometer is in the zero range and the door is closed, all lights are off
+void defaultState() {
+    digitalWrite(rLED, LOW);
+    digitalWrite(gLED, LOW);
+    digitalWrite(bLED, LOW);
+    checkDoor();
+}
+
+// the potentiometer is within the 1026-2050 range resulting in a green light turning on indicating the
+//user's choice of chicken for their meal
+void chickenState() {
+    digitalWrite(rLED, LOW);
+    digitalWrite(gLED, HIGH);
+    digitalWrite(bLED, LOW);
+    checkDoor();
+}
+
+// the potentiometer is within the 2051-3075 range resulting in a red light turning on indicating the
+//user's choice of cow for their meal
+void cowState() {
+    digitalWrite(rLED, HIGH);
+    digitalWrite(gLED, LOW);
+    digitalWrite(bLED, LOW);
+    checkDoor();
+}
+
+
+// the potentiometer is within the 3076-4095 range resulting in a blue light turning on indicating the
+//user's choice of fish for their meal
+void fishState() {
+    digitalWrite(rLED, LOW);
+    digitalWrite(gLED, LOW);
+    digitalWrite(bLED, HIGH);
+    checkDoor();
 }
--- Processing/Project3_Food/Project3_Food.pde
+++ Processing/Project3_Food/Project3_Food.pde
@@ -1,12 +1,64 @@
 /* Project 3 Food
 by Ashley Woon
 
+Made in connection with an Arduino sketch and hardware including:
+  (1)Adafruit ESP32, (1)breadboard, (1)push button, (1)potentiometer, (1)LED, (1) RGB LED, (5)resistors
 */
 
-void setup() {
+// Importing the serial library to communicate with the Arduino 
+import processing.serial.*;    
 
+// Initializing a vairable named 'myPort' for serial communication
+Serial myPort;      
+
+// Data coming in from the data fields
+// data[0] = "1" or "0"                  -- BUTTON
+// data[1] = 0-4095, e.g "2049"          -- POT VALUE
+// data[2] = 0-4095, e.g. "1023"        -- LDR value
+String [] data;
+
+int switchValue = 0;
+int potValue = 0;
+int ldrValue = 0;
+
+// Change to appropriate index in the serial list — YOURS MIGHT BE DIFFERENT
+int serialIndex = 0;
+
+void setup() {
+  // List all the available serial ports
+  printArray(Serial.list());
+  
+  // Set the com port and the baud rate according to the Arduino IDE
+  //-- use your port name
+  myPort  =  new Serial (this, "COM3",  115200); 
+  
+}
+
+// We call this to get the data 
+void checkSerial() {
+  while (myPort.available() > 0) {
+    String inBuffer = myPort.readString();  
+    
+    print(inBuffer);
+    
+    // This removes the end-of-line from the string 
+    inBuffer = (trim(inBuffer));
+    
+    // This function will make an array of TWO items, 1st item = switch value, 2nd item = potValue
+    data = split(inBuffer, ',');
+   
+   // we have THREE items — ERROR-CHECK HERE
+   if( data.length >= 2 ) {
+      switchValue = int(data[0]);           // first index = switch value 
+      potValue = int(data[1]);               // second index = pot value
+      ldrValue = int(data[2]);               // third index = LDR value
+      
+      // change the display timer
+      timePerLine = map( potValue, minPotValue, maxPotValue, minTimePerLine, maxTimePerLine );
+      displayTimer.setTimer( int(timePerLine));
+   }
 }
 
 void draw() {
-  
+  checkSerial();
 }
